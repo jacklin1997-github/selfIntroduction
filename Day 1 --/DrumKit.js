bdb324012f997069389fbca5fcd74469124c9903
albert <61795073+jacklin1997-github@users.noreply.github.com>
2022-06-11 17:41:04 +0800

    Add files via upload
    day 1

---
+++ Day 1 --/DrumKit.js
@@ -0,0 +1,22 @@
+function playMusic(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) {
+    console.log("無此按鍵");
+    return;
+  }
+
+  key.classList.add("playing");
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("playing");
+}
+
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+// The transitionend event occurs when a CSS transition has completed.
+window.addEventListener("keydown", playMusic);
